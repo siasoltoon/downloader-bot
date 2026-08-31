98e5053a54ce67caf22cdc8988c91c4875812acc
siasoltoon <siyavashjahangiri105@gmail.com>
2026-09-01 00:55:38 +0330

    feat: harden application configuration

--- app/config.py
+++ app/config.py
@@ -10,12 +10,15 @@ class Settings(BaseSettings):
     max_workers: int = 2
     max_active_jobs_per_user: int = 2
     max_file_size_mb: int = 2048
+    max_download_seconds: int = 3600
     storage_endpoint: str = ""
     storage_region: str = "auto"
     storage_bucket: str = ""
     storage_access_key: str = ""
     storage_secret_key: str = ""
+    storage_public_base_url: str = ""
     storage_presigned_ttl: int = 86400
+    log_level: str = "INFO"
     model_config = SettingsConfigDict(env_file=".env", env_prefix="", extra="ignore")
 
 
